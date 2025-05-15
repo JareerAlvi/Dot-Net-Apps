793715b15cb3f860bc5dc7b9460096bb1210bc41
MY GUEST <MY GUEST@DESKTOP-EC973PO>
2025-05-15 21:54:09 +0500

    .

--- WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb7d42cbccff97d05f20a8567535921604009703")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b240f7bc7dda83b9affa3513cdc19c2da5b0598")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
