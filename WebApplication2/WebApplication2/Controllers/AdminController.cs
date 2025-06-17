f34f923ef9a208f41e46d480adb5fb68e08371c3
MY GUEST <MY GUEST@DESKTOP-EC973PO>
2025-06-17 22:02:10 +0300

    .

--- WebApplication2/WebApplication2/Controllers/AdminController.cs
+++ WebApplication2/WebApplication2/Controllers/AdminController.cs
@@ -44,43 +44,100 @@ namespace WebApplication2.Controllers
         public IActionResult UpdateUser(string userId, string email, string userName, string password, string[] selectedRoles)
         {
             var user = _userManager.FindByIdAsync(userId).Result;
-          user.Email=email;
-            user.UserName=userName;
+
+            var emailResult = _userManager.SetEmailAsync(user, email).Result;
+            if (!emailResult.Succeeded)
+            {
+                foreach (var error in emailResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            user.UserName = email;
+
             if (!string.IsNullOrEmpty(password))
             {
                 var token = _userManager.GeneratePasswordResetTokenAsync(user).Result;
                 var passwordResult = _userManager.ResetPasswordAsync(user, token, password).Result;
+
+                if (!passwordResult.Succeeded)
+                {
+                    foreach (var error in passwordResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
             }
-                var updateResult = _userManager.UpdateAsync(user).Result;
-            //Chechikng what roles does the slected user has rn
-            var currentRoles= _userManager.GetRolesAsync(user).Result;
-            //Checking what new roles were selected by admin to add 
+
+            var updateResult = _userManager.UpdateAsync(user).Result;
+            if (!updateResult.Succeeded)
+            {
+                foreach (var error in updateResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            if (!ModelState.IsValid)
+            {
+                var errorList = ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .Where(msg => !string.IsNullOrWhiteSpace(msg))  // filter out empty messages
+                    .ToList();
+
+                if (errorList.Any())
+                {
+                    TempData["Errors"] = string.Join("|||", errorList);
+                }
+
+                return RedirectToAction("AdminDashboard", new { selectedUserId = user.Id });
+            }
+
+            // Update roles
+            var currentRoles = _userManager.GetRolesAsync(user).Result;
             var rolesToAdd = selectedRoles.Except(currentRoles);
-            //Checking what new roles were selected by admin to remove
             var rolesToRemove = currentRoles.Except(selectedRoles);
             _userManager.AddToRolesAsync(user, rolesToAdd).Wait();
             _userManager.RemoveFromRolesAsync(user, rolesToRemove).Wait();
 
             return RedirectToAction("AdminDashboard");
-        
-
         }
+
+
 
         [HttpPost]
         public IActionResult DeleteUser(string userId)
         {
             var user = _userManager.FindByIdAsync(userId).Result;
+            if(userId==)
             _userManager.DeleteAsync(user).Wait();
             return RedirectToAction("AdminDashboard");
 
         }
 
         [HttpPost]
-        public IActionResult CreateUser(string email,  string password, string[] selectedRoles)
+        [HttpPost]
+        public IActionResult CreateUser(string email, string password, string[] selectedRoles)
         {
-            var newUser = new IdentityUser { Email = email, UserName = email};
+            var newUser = new IdentityUser { Email = email, UserName = email };
             var result = _userManager.CreateAsync(newUser, password).Result;
 
+            if (!result.Succeeded)
+            {
+                var errors = result.Errors
+                    .Select(e => e.Description)
+                    .Where(msg => !string.IsNullOrWhiteSpace(msg))
+                    .ToList();
+
+                if (errors.Any())
+                {
+                    TempData["Errors"] = string.Join("|||", errors);
+                }
+
+                return RedirectToAction("AdminDashboard");
+            }
 
 
             if (selectedRoles?.Length > 0)
@@ -90,5 +147,6 @@ namespace WebApplication2.Controllers
 
             return RedirectToAction("AdminDashboard");
         }
+
     }
 }
